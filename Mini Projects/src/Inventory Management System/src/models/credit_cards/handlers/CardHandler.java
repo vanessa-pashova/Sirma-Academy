514816db8212f1e7c8cb900849a4850eaaad3e24
vanessa-pashova <vanesapashova16@gmail.com>
2024-12-22 17:10:26 +0200

    email for verification added

--- Mini Projects/src/Inventory Management System/src/models/credit_cards/handlers/CardHandler.java
+++ Mini Projects/src/Inventory Management System/src/models/credit_cards/handlers/CardHandler.java
@@ -8,22 +8,23 @@ import models.credit_cards.payment_systems.Visa;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CardHandler implements CardHandlerInterface {
-    private Map<String, AbstractCard> cards;
+    private TreeMap<String, AbstractCard> cards;
     private final String FILE = "/Users/vanessa.pashova/Desktop/Sirma Academy 24/Mini Projects/src/Inventory Management System/src/csv_files/credit_cards/Credentials.csv";
 
     public CardHandler() {
-        cards = new HashMap<>();
+        cards = new TreeMap<>();
     }
 
     @Override
-    public Map<String, AbstractCard> loadCards() {
+    public TreeMap<String, AbstractCard> loadCards() {
         File file = new File(this.FILE);
         if(!file.exists()) {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-               writer.write("PaymentSystem|CardNumber|ExpiryDate|CCV|FirstName|FamilyName");
-               writer.newLine();
+                writer.write("PaymentSystem|CardNumber|ExpiryDate|CCV|FirstName|FamilyName|Balance|Email");
+                writer.newLine();
             } catch (IOException e) {
                 System.out.print(">! Error while creating loading file! [CardHandler, loadCards()].");
                 System.out.println(e.getMessage());
@@ -43,21 +44,22 @@ public class CardHandler implements CardHandlerInterface {
                 String firstName = components[4];
                 String familyName = components[5];
                 double amount = Double.parseDouble(components[6]);
+                String email = components[7];
 
                 AbstractCard card = null;
                 if(paymentSystem.equalsIgnoreCase("Visa"))
-                    card = new Visa(firstName, familyName, cardNumber, expiryDate, ccv, paymentSystem, amount);
+                    card = new Visa(firstName, familyName, email, cardNumber, expiryDate, ccv, paymentSystem, amount);
 
                 else if(paymentSystem.equalsIgnoreCase("MasterCard"))
-                    card = new MasterCard(firstName, familyName, cardNumber, expiryDate, ccv, paymentSystem, amount);
+                    card = new MasterCard(firstName, familyName, email, cardNumber, expiryDate, ccv, paymentSystem, amount);
 
                 else if(paymentSystem.equalsIgnoreCase("AmericanExpress"))
-                    card = new AmericanExpress(firstName, familyName, cardNumber, expiryDate, ccv, paymentSystem, amount);
+                    card = new AmericanExpress(firstName, familyName, email, cardNumber, expiryDate, ccv, paymentSystem, amount);
 
                 else
                     throw new IllegalArgumentException(">! Unknown payment system [" + paymentSystem + "] [CardHandler, loadCards()].");
 
-                cards.put(cardNumber, card);
+                cards.put(email, card);
             }
         } catch (IOException e) {
             System.out.print(">! Error while reading file! [CardHandler, loadCards()].");
@@ -88,15 +90,15 @@ public class CardHandler implements CardHandlerInterface {
     @Override
     public void saveCards() {
         File file = new File(this.FILE);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             if (file.length() == 0) {
-                writer.write("PaymentSystem|CardNumber|ExpiryDate|CCV|FirstName|FamilyName");
+                writer.write("PaymentSystem|CardNumber|ExpiryDate|CCV|FirstName|FamilyName|Balance|Email");
                 writer.newLine();
             }
 
             cards.forEach((cardNumber, card) -> {
                 try {
-                    writer.write(card.getClass().getSimpleName() + "|" + card.getNumber() + "|" + card.getExpiryDate() + "|" + card.getCCV() + "|" + card.getFirstName() + "|" + card.getFamilyName() + "|" + card.getBalance());
+                    writer.write(card.getClass().getSimpleName() + "|" + card.getNumber() + "|" + card.getExpiryDate() + "|" + card.getCCV() + "|" + card.getFirstName() + "|" + card.getFamilyName() + "|" + card.getBalance() + "|" + card.getEmail());
                     writer.newLine();
                 } catch (IOException e) {
                     System.out.print(">! Error while saving in Credentials.csv! [CardHandler, saveCards()].");
@@ -120,7 +122,7 @@ public class CardHandler implements CardHandlerInterface {
         cards.put(card.getNumber(), card);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE, true))) {
-            writer.write(card.getClass().getSimpleName() + "|" + card.getNumber() + "|" + card.getExpiryDate() + "|" + card.getCCV() + "|" + card.getFirstName() + "|" + card.getFamilyName() + "|" + card.getBalance());
+            writer.write(card.getClass().getSimpleName() + "|" + card.getNumber() + "|" + card.getExpiryDate() + "|" + card.getCCV() + "|" + card.getFirstName() + "|" + card.getFamilyName() + "|" + card.getBalance() + "|" + card.getEmail());
             writer.newLine();
         } catch (IOException e) {
             System.out.print(">! Error while saving the card in Credentials.csv! [CardHandler, addCard()].");
@@ -128,35 +130,10 @@ public class CardHandler implements CardHandlerInterface {
         }
     }
 
-    public void addAmount(String cardNumber, double amount) {
-        if(amount <= 0)
-            throw new IllegalArgumentException(">! Amount cannot be less or equal than zero!, [CardHandler, addAmount()].");
-
-        if(cards.containsKey(cardNumber)) {
-            this.cards.get(cardNumber).setBalance(cards.get(cardNumber).getBalance() + amount);
-            removeAllCards();
-            saveCards();
-        }
-
-        else
-            throw new IllegalArgumentException(">! Card does not exist, [CardHandler, addAmount()].");
-    }
-
-    public void sendMoney(String cardNumber, double amount) {
-        if(cards.containsKey(cardNumber)) {
-            this.cards.get(cardNumber).setBalance(cards.get(cardNumber).getBalance() - Math.abs(amount));
-            removeAllCards();
-            saveCards();
-        }
-
-        else
-            throw new IllegalArgumentException(">! Card does not exist! [CardHandler, sendMoney()].");
-    }
-
     public void removeAllCards() {
         File file = new File(this.FILE);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write("PaymentSystem|CardNumber|ExpiryDate|CCV|FirstName|FamilyName|Balance");
+            writer.write("PaymentSystem|CardNumber|ExpiryDate|CCV|FirstName|FamilyName|Balance|Email");
             writer.newLine();
         } catch (IOException e) {
             System.out.print(">! Error while clearing the file Credentials.csv! [CardHandler, removeAllCards()].");
